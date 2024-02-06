4eff5dcc5af8e601a6d0e1a49de54ada6a47f3ba
smbock42 <smbock42@gmail.com>
2024-02-06 14:25:56 -0800

    Add AES CBC encryption and decryption functions

--- assignment3/task3.py
+++ assignment3/task3.py
@@ -1,4 +1,8 @@
 from Crypto.Util.number import getPrime, inverse
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
 
 def generate_key(bits):
     e = 65537
@@ -15,59 +19,38 @@ def generate_key(bits):
     private_key = (n, d)
     return public_key, private_key
     
-    
-def encrypt(message, public_key):
-    message = message.encode('utf-8')
-    int_message = int.from_bytes(message, byteorder='big')
-    encrypted = pow(int_message, public_key[1], public_key[0])
-    return encrypted
+def aes_cbc_encrypt(key, plaintext):
+    cipher = AES.new(key, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(plaintext.encode(), AES.block_size))
+    iv = cipher.iv
+    return iv + ct_bytes
 
-def decrypt(encrypted_message, private_key):
-    decrypted = pow(encrypted_message, private_key[1], private_key[0])
-    decrypted = decrypted.to_bytes((decrypted.bit_length() + 7) // 8, byteorder='big')
-    try:
-        decrypted = decrypted.decode('utf-8')
-    except UnicodeDecodeError:
-        decrypted = str(decrypted)
-    return decrypted
+def aes_cbc_decrypt(key, ciphertext):
+    iv = ciphertext[:AES.block_size]
+    ct = ciphertext[AES.block_size:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    pt = unpad(cipher.decrypt(ct), AES.block_size)
+    return pt.decode()
 
 def main():
     # public_key[0] = n, public_key[1] = e, private_key[0] = n, private_key[1] = d
     public_key, private_key = generate_key(256)
     message = 'Hi!'
     print('Original message:', message)
-    encrypted_message = encrypt(message, public_key)
-        
-    decrypted_message = decrypt(encrypted_message, private_key)
-    print('Decrypted message:', decrypted_message)
     
-    # mallory intercepts the message and modifies it c' = F(c)
-    k = 2
-
-    # c = m^3 mod n where m is original message
+    # mallory sends alice a new c_prime
+    c_prime = 1
     
-    # k^e mod n - encrypt k 
-    k_encrypted = pow(k, public_key[1], public_key[0])
+    s = pow(c_prime, private_key[1], private_key[0])
     
-    #modified ciphertext
-    c_prime = 0
+    k = SHA256.new()
+    k.update(str(s).encode())
+    c_0 = aes_cbc_encrypt(k.digest(), message)
     
-    print('Mallory intercepts the message and modifies it: ', c_prime)
+    c = aes_cbc_decrypt(k.digest(), c_0)
+    print("mallory modified decrypted: ", c)
     
-    #decrypt modified message - when decrypted, it will be m * k
-    # modified_decrypted_message = decrypt(c_prime, private_key)
-    pow(c_prime, private_key[1], private_key[0])
-    modified_decrypted_message = int.from_bytes(modified_decrypted_message.encode('utf-8'), byteorder='big') // k
-    modified_decrypted_message = modified_decrypted_message.to_bytes((modified_decrypted_message.bit_length() + 7) // 8, byteorder='big')
-
-    print('Decrypted modified message:', modified_decrypted_message)
-
-    # k_encrypted = pow(k, public_key[1], public_key[0])
-    # c_prime = (encrypted_message * k_encrypted) % public_key[0]
-    # print('Mallory intercepts the message and modifies it: ', c_prime)
-    # modified_decrypted_message = decrypt(c_prime, public_key)
-    # print('Decrypted modified message:', modified_decrypted_message)
-        
     
 if __name__ == "__main__":
     main()
+    
